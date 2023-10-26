47267565040c469f3b5962582180f55b40d0d813
majudlorenzoni <juliamaria892@gmail.com>
2023-10-26 13:00:20 -0300

    Exercício de criação do triângulo

--- testes/public/main.js
+++ testes/public/main.js
@@ -1,8 +1,48 @@
+console.log("funcionando");
+
 const canvas = document.querySelector('canvas');
 const gl = canvas.getContext('webgl');
 
 if (!gl){
     throw new Error ('WebGL not supported');
 }
+console.log("funcionando");
 
-alert(`Everything's peachy hear with WebGL`)
+const vertexData = [
+    0, 1, 0,
+    1, -1, 0,
+    -1, -1, 0,
+]; //é a parte mais simples
+
+const buffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.STATIC_DRAW);
+
+const vertexShader = gl.createShader(gl.VERTEX_SHADER); // mini programa que roda na gpu
+gl.shaderSource(vertexShader, `
+attribute vec3 position;
+void main(){
+    gl_Position = vec4(position, 1);
+}
+`);
+gl.compileShader(vertexShader);
+
+const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+gl.shaderSource(fragmentShader, `
+void main(){
+    gl_FragColor = vec4(1, 0, 0, 1);
+}
+`);
+gl.compileShader(fragmentShader);
+
+const program = gl.createProgram();
+gl.attachShader(program, vertexShader);
+gl.attachShader(program, fragmentShader);
+gl.linkProgram(program);
+
+const positionLocation = gl.getAttribLocation(program, `position`);
+gl.enableVertexAttribArray(positionLocation);
+gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
+
+gl.useProgram(program);
+gl.drawArrays(gl.TRIANGLES, 0, 3);
